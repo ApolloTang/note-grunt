944add7129e1624e565edc67388af966ff3d63fb
Apollo Tang <tang.apollo@gmail.com>
2015-01-18 16:36:48 -0500

    c_tut+gettingl-good | add study for video 1.3

---
+++ c_tut+_getting-good-w-grunt/1.3/wf/Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        // https://github.com/gruntjs/grunt-contrib-uglify
+        uglify: { // <-- short name for contrib plugin
+            options: {
+                mangle: true,
+                compress: true,
+                sourceMap: true,
+                sourceMapName: 'dist/app.map',
+                banner: "/* Apollo Tang 2015 */\n"
+            },
+            target_1: {
+                files: {
+                    "dist/app.min.js": ["src/app_1.js", "src/app_2.js"]
+                }
+            },
+            target_2: {
+                files: {
+                    "dist/util.min.js": ["src/util.js"]
+                }
+            }
+        }
+    })
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+}
